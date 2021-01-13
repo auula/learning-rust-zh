4e12b80849987e2f4cecf0e429f4c3b7b285703c
Dings <deen.job@qq.com>
2021-01-13 16:52:56 +0800

    remove target

--- hello_cargo/target/debug/deps/hello_cargo-78eae60a3411fe6a.d
+++
@@ -1,5 +0,0 @@
-/Users/ding/Desktop/learning-rust-zh/hello_cargo/target/debug/deps/hello_cargo-78eae60a3411fe6a.rmeta: src/main.rs
-
-/Users/ding/Desktop/learning-rust-zh/hello_cargo/target/debug/deps/hello_cargo-78eae60a3411fe6a.d: src/main.rs
-
-src/main.rs:
--- hello_cargo/target/debug/deps/hello_cargo.d
+++
@@ -1,5 +0,0 @@
-/Users/ding/Desktop/learning-rust-zh/hello_cargo/target/debug/deps/hello_cargo: src/main.rs
-
-/Users/ding/Desktop/learning-rust-zh/hello_cargo/target/debug/deps/hello_cargo.d: src/main.rs
-
-src/main.rs:
