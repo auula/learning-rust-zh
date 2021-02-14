4f48f01b6fec203749abb91b71dec161aa641088
Dings <deen.job@qq.com>
2021-02-14 22:20:08 +0800

    add tuple example

---
+++ tuple/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let tuples: (&'static str, i8, f64) = ("🦀", 22, 3.1415927);
+    println!("{:?}", tuples);
+    // 声明一个可变的tuple
+    let mut people = ("tom", "robin", "jarvib");
+    // 通过下标访问
+    println!("{},{},{}", people.0, people.1, people.2);
+    // 修改下标为2的值
+    people.2 = "Jarvib Ding";
+    let (v1, v2, v3) = people;
+    println!("{},{},{}", v1, v2, v3)
+}
